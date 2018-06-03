e418b049f1e9c0daf8bb13c3533f5e95962ca0e8
staxx6 <staxx@hotmail.de>
2018-06-03 20:22:06 +0200

    update animations and transitions added more css vars

--- js/color.js
+++ js/color.js
@@ -1,8 +1,13 @@
-let test = () => {
-    //document.getElementsByName('body').style.backgroundcolor 
+let shake = () => {
     document.documentElement.style.setProperty('--color-main', 'red');
 
-    let colorBlack = getComputedStyle(document.documentElement.style.getPropertyValue('--color-black'));
-    document.documentElement.style.setProperty('--color-shake', document.documentElement.get);
-    document.documentElement.style.setProperty('--color-for', 'red');
+    let colorBlack = window.getComputedStyle(document.body).getPropertyValue('--color-black');
+    document.documentElement.style.setProperty('--color-shake', colorBlack);
+    document.documentElement.style.setProperty('--color-for', colorBlack);
+    document.documentElement.style.setProperty('--color-color-arrow', colorBlack);
+
+    let animationTime = 1.4;
+    document.getElementsByClassName('main-text__color')[0].style.animation = `shake-color ${animationTime}s ease-in-out`;
+
+    // document.getElementsByClassName('main-text__color')[0].style.animation = ``;
 }
