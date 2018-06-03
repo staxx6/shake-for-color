f367069bff17c82a58f5a99883b72a570e042402
staxx6 <staxx@hotmail.de>
2018-06-03 23:23:51 +0200

    fixed some color changes and added color for arrows

--- js/color.js
+++ js/color.js
@@ -1,9 +1,13 @@
 let shake = () => {
-    document.documentElement.style.setProperty('--color-main', 'red');
+    let newColor = 'red';
 
+    document.documentElement.style.setProperty('--color-main', newColor);
+
+    //TODO get value from newColor and decide if it should be white or black text color
     let colorBlack = window.getComputedStyle(document.body).getPropertyValue('--color-black');
     document.documentElement.style.setProperty('--color-shake', colorBlack);
     document.documentElement.style.setProperty('--color-for', colorBlack);
+    document.documentElement.style.setProperty('--color-color', newColor); // needs to wait
     document.documentElement.style.setProperty('--color-color-arrow', colorBlack);
 
     let animationTime = 1.4;
